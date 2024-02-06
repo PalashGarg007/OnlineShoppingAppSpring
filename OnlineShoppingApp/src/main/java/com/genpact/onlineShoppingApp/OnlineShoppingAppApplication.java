659b362c4dcaa6e4a7aae241c9539eb52b7adc23
Palash garg <gargpalash71@gmail.com>
2024-02-06 18:26:12 +0530

    .

--- OnlineShoppingApp/src/main/java/com/genpact/onlineShoppingApp/OnlineShoppingAppApplication.java
+++ OnlineShoppingApp/src/main/java/com/genpact/onlineShoppingApp/OnlineShoppingAppApplication.java
@@ -1,10 +1,16 @@
 package com.genpact.onlineShoppingApp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.genpact.onlineShoppingApp.service.AdminService;
@@ -21,13 +27,15 @@ public class OnlineShoppingAppApplication{
 	@Autowired
 	private VendorService vendorService;
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	 String home() {
 		return "Hello World!";
 	 }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineShoppingAppApplication.class, args);
+		
+//		streamExample();
 	}
 
 //	@Override
@@ -43,7 +51,24 @@ public class OnlineShoppingAppApplication{
 //		vendorService.addNewProduct();
 //		vendorService.getProducts(0);
 //		vendorService.changePersonalInformadtion();
+	}
+	
+	private static void streamExample() {
+		List<Integer> li = Arrays.asList(1,3,5,6,2,4,8,7,7,7);
 		
+		List<Integer> newStreamList = li.stream()
+				.map(x -> x*x)
+				.filter(x -> x<30)
+				.sorted((o1, o2) -> (o1>o2)?-1:((o2>o1)?+1:0))
+				.collect(Collectors.toList());
+		
+		Integer newStreamValue = li.stream()
+				.filter(x -> x%2==0)
+				.reduce(0, (value, x) -> value+x);
+		
+		System.out.println("Sum of all even number of:" + li + " is: " + newStreamValue);
+		System.out.println(newStreamList);
+		newStreamList.forEach(x -> System.out.println(x));
 	}
 
 }
